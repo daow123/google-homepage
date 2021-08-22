61aadc86aa3d35f0cb456214111990ddf8aa6a41
daow123 <daow123@protonmail.com>
2021-08-22 21:25:09 +0700

    add input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import SearchBox from './SearchBox';
 
 function App() {
   return (
     <div className="App">
+      <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="google" />
+      <SearchBox /> 
     </div>
   );
 }
---
+++ src/SearchBox.js
@@ -0,0 +1,7 @@
+
+export default function SearchBox() {
+  return <>
+    <span className="material-icons orange600">face</span>
+    <input type="text" id="searhbox" />
+  </>;
+}
